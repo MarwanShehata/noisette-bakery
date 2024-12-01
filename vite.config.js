09f888596b1d961b1e6a3504bec046c02a1aa729
Marwan Shehata <marwanzeitoun1@gmail.com>
2024-12-01 05:04:11 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                contactUs: resolve(__dirname, 'contact-us.html'),
+                location: resolve(__dirname, 'location.html'),
+                menu: resolve(__dirname, 'menu.html')
+            }
+        }
+    },
+    // Optional: Add public base path for Render deployment
+    base: '/'
+});
